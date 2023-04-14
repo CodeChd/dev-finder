be7da69da2f6f32e7d3ac48d502c92b83a6410a8
CodeChd <Drowzyking1126@gmail.com>
2023-04-14 16:50:57 +0800

    Html structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import SearchIcon from "../public/assets/icon-search.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <p>devfinder</p>
+        <div className="Modes">
+          <p>Light </p>
+        </div>
+      </header>
+
+      <form>
+        <img src={SearchIcon} alt="" />
+        <input type="text" placeholder="Search Github Username" />
+        <button>Search</button>
+      </form>
+
+      <main className="content">
+        <div className="circle"></div>
+
+        <div className="details">
+          <div className="description">
+            <p>The Octocat</p>
+            <p>@octocat</p>
+            <p>This profile has no bio</p>
+          </div>
+          <div className="date">
+            <p>Joined jan 25 2011</p>
+          </div>
+
+          <div className="profile_social">
+            <p>
+              Repos
+              <span>8</span>
+            </p>
+            <p>
+              Followers
+              <span>3839</span>
+            </p>
+            <p>
+              Following
+              <span>9</span>
+            </p>
+          </div>
+
+          <div className="desc-1">
+            <p>San Francisco</p>
+            <p>https://github.blog</p>
+          </div>
+          <div className="desc-2">
+            <p>Not available</p>
+            <p>@github</p>
+          </div>
+        </div>
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
